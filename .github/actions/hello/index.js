3c31dab98dcc494359b09e43f37bf49e675ffda8
mehenna misoum <mehenna.missoum.cka@gmail.com>
2024-06-22 19:30:35 +0200

    feat: firt github action

---
+++ .github/actions/hello/index.js
@@ -0,0 +1,21 @@
+import core from '@actions/core';
+import github from '@actions/github';
+
+try {
+
+    core.debug('Hello, World!');
+    const name = core.getInput('who_to_greet');
+
+    core.debug('Hello,  ${name}!');
+    const time = new Date();
+    core.setOutput('time', time.toTimeString());
+
+    core.exportVariable('HELLO_TIME', time);    
+
+    core.startGroup('Logging github context');
+    console.log(JSON.stringify(github.context, null, 1));
+    core.endGroup();
+
+} catch (error) {
+    core.setFailed(error.message);
+}
